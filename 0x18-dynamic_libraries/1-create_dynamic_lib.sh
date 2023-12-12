c07bf9f90613170a6432dd95bf5cf48a8bd456f4
RozaKassa <rosepinkyyy33.com>
2023-12-12 14:00:09 -0800

    my commit

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c  -fPIC *.c
-gcc -shared *.o -o liball.so *.o
-export LD_LIBRARY_PATH = .:$LD_LIBRARY_PATH
+gcc -fPIC -c *.c
+gcc -shared *.o -o liball.so
 
 
 
